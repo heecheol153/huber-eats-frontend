16191604e1794875743abb9db496b9aa3faeb187
heecheol153 <heecheol153@gmail.com>
2021-09-01 23:58:43 +0900

    #19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,22 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Huber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Huber Eats");
+  });
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("heech@untieall.com")
+      .findByPlaceholderText(/password/i)
+      .type("12345")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none");
+    // to do (can log in)
+  });
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+      .findByRole("alert")
+      .should("have.text", "Please enter a valid email");
   });
 });
