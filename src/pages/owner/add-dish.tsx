c5614e98c88c352bfe9bfa9a7fd8e05937d61a0a
heecheol153 <heecheol153@gmail.com>
2021-09-06 17:05:41 +0900

    #20.9 Create Dish part Two

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,6 +1,14 @@
 import { gql, useMutation } from "@apollo/client";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router-dom";
+import { Button } from "../../components/button";
+import {
+  createDish,
+  createDishVariables,
+} from "../../__generated__/createDish";
+import { MY_RESTAURANTS_QUERY } from "./my-restaurants";
 
 const CREATE_DISH_MUTATION = gql`
   mutation createDish($input: CreateDishInput!) {
@@ -15,8 +23,85 @@ interface IParams {
   restaurantId: string;
 }
 
+interface IForm {
+  name: string;
+  price: string;
+  description: string;
+}
+
 export const AddDish = () => {
   const { restaurantId } = useParams<IParams>();
-  const [createDishMutation, { loading }] = useMutation(CREATE_DISH_MUTATION);
-  return <h1>a</h1>;
+  const history = useHistory();
+  const [createDishMutation, { loading }] = useMutation<
+    createDish,
+    createDishVariables
+  >(CREATE_DISH_MUTATION, {
+    refetchQueries: [
+      {
+        query: MY_RESTAURANTS_QUERY,
+        variables: {
+          input: {
+            id: +restaurantId,
+          },
+        },
+      },
+    ],
+  });
+  const { register, handleSubmit, formState, getValues } = useForm<IForm>({
+    mode: "onChange",
+  });
+  const onSubmit = () => {
+    const { name, price, description } = getValues();
+    createDishMutation({
+      variables: {
+        input: {
+          name,
+          price: +price,
+          description,
+          restaurantId: +restaurantId,
+        },
+      },
+    });
+    history.goBack();
+  };
+  return (
+    <div className="container flex flex-col items-center mt-52">
+      <Helmet>
+        <title>Add Dish | Nuber Eats</title>
+      </Helmet>
+      <h4 className="font-semibold text-2xl mb-3">Add Dish</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+      >
+        <input
+          className="input"
+          type="text"
+          placeholder="Name"
+          {...register("name", { required: "Name is required." })}
+          name="name"
+        />
+        <input
+          className="input"
+          type="number"
+          min={0} //음수가나오면안되니까 최소값0로, -10식으로 하면 graphQL에 장애발생가능
+          placeholder="Price"
+          {...register("price", { required: "Price is required." })}
+          name="price"
+        />
+        <input
+          className="input"
+          type="text"
+          placeholder="Description"
+          {...register("description", { required: "Description is required." })}
+          name="description"
+        />
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Create Dish"
+        />
+      </form>
+    </div>
+  );
 };
