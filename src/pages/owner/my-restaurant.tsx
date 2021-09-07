ed7faf96c4fea93831f0411e36621783f760843d
heecheol153 <heecheol153@gmail.com>
2021-09-07 13:33:24 +0900

    #21.2 Paddle Integration part One

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,19 +1,8 @@
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
-import {
-  VictoryAxis,
-  VictoryBar,
-  VictoryChart,
-  VictoryLabel,
-  VictoryLine,
-  VictoryPie,
-  VictoryTheme,
-  VictoryTooltip,
-  VictoryVoronoiContainer,
-} from "victory";
 import {
   DISH_FRAGMENT,
   ORDERS_FRAGMENT,
@@ -23,10 +12,6 @@ import {
   myRestaurant,
   myRestaurantVariables,
 } from "../../__generated__/myRestaurant";
-import {
-  createPayment,
-  createPaymentVariables,
-} from "../../__generated__/createPayment";
 import { useMe } from "../../hooks/useMe";
 
 export const MY_RESTAURANT_QUERY = gql`
@@ -47,16 +32,6 @@ export const MY_RESTAURANT_QUERY = gql`
   }
   ${RESTAURANT_FRAGMENT}
   ${DISH_FRAGMENT}
-  ${ORDERS_FRAGMENT}
-`;
-
-const CREATE_PAYMENT_MUTATION = gql`
-  mutation createPayment($input: CreatePaymentInput!) {
-    createPayment(input: $input) {
-      ok
-      error
-    }
-  }
 `;
 
 interface IParams {
@@ -75,54 +50,18 @@ export const MyRestaurant = () => {
       },
     }
   );
-  console.log(data);
-  const onCompleted = (data: createPayment) => {
-    if (data.createPayment.ok) {
-      alert("Your restaurant is being promoted!");
-    }
-  };
-  const [createPaymentMutation, { loading }] = useMutation<
-    createPayment,
-    createPaymentVariables
-  >(CREATE_PAYMENT_MUTATION, {
-    onCompleted,
-  });
   const { data: userData } = useMe();
   const triggerPaddle = () => {
     if (userData?.me.email) {
       // @ts-ignore
-      window.Paddle.Setup({ vendor: 666 });
+      window.Paddle.Setup({ vendor: 31465 });
       // @ts-ignore
       window.Paddle.Checkout.open({
-        product: 666,
+        product: 63879,
         email: userData.me.email,
-        successCallback: (data: any) => {
-          createPaymentMutation({
-            variables: {
-              input: {
-                transactionId: data.checkout.id,
-                restaurantId: +id,
-              },
-            },
-          });
-        },
       });
     }
   };
-  const chartData = [
-    { x: 1, y: 3000 },
-    { x: 2, y: 1500 },
-    { x: 3, y: 4250 },
-    { x: 4, y: 1250 },
-    { x: 5, y: 2300 },
-    { x: 6, y: 7150 },
-    { x: 7, y: 6830 },
-    { x: 8, y: 6830 },
-    { x: 9, y: 6830 },
-    { x: 10, y: 6830 },
-    { x: 11, y: 6830 },
-  ];
-  console.log(data);
   return (
     <div>
       <Helmet>
@@ -169,48 +108,6 @@ export const MyRestaurant = () => {
               ))}
             </div>
           )}
-        </div>
-        <div className="mt-20 mb-10">
-          <h4 className="text-center text-2xl font-medium">Sales</h4>
-          <div className="  mt-10">
-            <VictoryChart
-              height={500}
-              theme={VictoryTheme.material}
-              width={window.innerWidth}
-              domainPadding={50}
-              containerComponent={<VictoryVoronoiContainer />}
-            >
-              <VictoryLine
-                labels={({ datum }) => `$${datum.y}`}
-                labelComponent={
-                  <VictoryTooltip
-                    style={{ fontSize: 18 } as any}
-                    renderInPortal
-                    dy={-20}
-                  />
-                }
-                data={data?.myRestaurant.restaurant?.orders.map((order) => ({
-                  x: order.createdAt,
-                  y: order.total,
-                }))}
-                interpolation="natural"
-                style={{
-                  data: {
-                    strokeWidth: 5,
-                  },
-                }}
-              />
-              <VictoryAxis
-                tickLabelComponent={<VictoryLabel renderInPortal />}
-                style={{
-                  tickLabels: {
-                    fontSize: 20,
-                  } as any,
-                }}
-                tickFormat={(tick) => new Date(tick).toLocaleDateString("ko")}
-              />
-            </VictoryChart>
-          </div>
         </div>
       </div>
     </div>
