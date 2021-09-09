fb8e3b04fbc2a100d112c5caa1b26c8320982d76
heecheol153 <heecheol153@gmail.com>
2021-09-09 22:36:44 +0900

    #23.1 Subscription Setup

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,17 +3,30 @@ import {
   createHttpLink,
   InMemoryCache,
   makeVar,
+  split,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN); //token값을얻다(null)
 export const isLoggedInVar = makeVar(Boolean(token)); //(null)을boolean값으로변환(false)
 export const authTokenVar = makeVar(token); //token변수 makeVar를 해주고 기본값을갖는다
 
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+    connectionParams: {
+      "x-jwt": authTokenVar() || "",
+    },
+  },
+});
+
 //각기본값을 본다.
-console.log("default value of isLoggedInVar is:", isLoggedInVar());
-console.log("default value of authToken is:", authTokenVar());
+//console.log("default value of isLoggedInVar is:", isLoggedInVar());
+//console.log("default value of authToken is:", authTokenVar());
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
@@ -29,8 +42,20 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
