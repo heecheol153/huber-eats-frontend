bdc1c0af4e5d729683320af41955ecd4f6eb3174
heecheol153 <heecheol153@gmail.com>
2021-09-10 14:43:05 +0900

    #23.4 Edit Order

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,11 +1,13 @@
-import { gql, useQuery, useSubscription } from "@apollo/client";
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
 import React from "react";
 import { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router";
 import { FULL_ORDER_FRAGMENT } from "../fragments";
 import { useMe } from "../hooks/useMe";
+import { editOrder, editOrderVariables } from "../__generated__/editOrder";
 import { getOrder, getOrderVariables } from "../__generated__/getOrder";
+import { OrderStatus, UserRole } from "../__generated__/globalTypes";
 import {
   orderUpdates,
   orderUpdatesVariables,
@@ -32,12 +34,24 @@ const ORDER_SUBSCRIPTION = gql`
   }
   ${FULL_ORDER_FRAGMENT}
 `;
+
+const EDIT_ORDER = gql`
+  mutation editOrder($input: EditOrderInput!) {
+    editOrder(input: $input) {
+      ok
+      error
+    }
+  }
+`;
 interface IParams {
   id: string;
 }
 export const Order = () => {
   const params = useParams<IParams>();
   const { data: userData } = useMe();
+  const [editOrderMutation] = useMutation<editOrder, editOrderVariables>(
+    EDIT_ORDER
+  );
   const { data, subscribeToMore } = useQuery<getOrder, getOrderVariables>(
     GET_ORDER,
     {
@@ -76,6 +90,16 @@ export const Order = () => {
       });
     }
   }, [data]);
+  const onButtonClick = (newStatus: OrderStatus) => {
+    editOrderMutation({
+      variables: {
+        input: {
+          id: +params.id,
+          status: newStatus,
+        },
+      },
+    });
+  };
   return (
     <div className="mt-32 container flex justify-center">
       <Helmet>
@@ -112,14 +136,30 @@ export const Order = () => {
               Status:{data?.getOrder.order?.status}
             </span>
           )}
-          {userData?.me.role === "Owner" && (
+          {userData?.me.role === UserRole.Owner && (
             <>
-              {data?.getOrder.order?.status === "Pending" && (
-                <button className="btn">Accept Order</button>
+              {data?.getOrder.order?.status === OrderStatus.Pending && (
+                <button
+                  onClick={() => onButtonClick(OrderStatus.Cooking)}
+                  className="btn"
+                >
+                  Accept Order
+                </button>
               )}
-              {data?.getOrder.order?.status === "Cooking" && (
-                <button className="btn">Order Cooked</button>
+              {data?.getOrder.order?.status === OrderStatus.Cooking && (
+                <button
+                  onClick={() => onButtonClick(OrderStatus.Cooked)}
+                  className="btn"
+                >
+                  Order Cooked
+                </button>
               )}
+              {data?.getOrder.order?.status !== OrderStatus.Cooking &&
+                data?.getOrder.order?.status !== OrderStatus.Pending && (
+                  <span className="text-center mt-5 mb-3 text-2xl text-lime-600">
+                    Status:{data?.getOrder.order?.status}
+                  </span>
+                )}
             </>
           )}
         </div>
