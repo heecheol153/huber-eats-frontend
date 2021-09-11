8b0e6f09d649ff5cf2ce1d2509e3520860e77135
heecheol153 <heecheol153@gmail.com>
2021-09-11 15:55:18 +0900

    #23.8 Address Geocoding

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { useEffect, useState } from "react";
+import googleMapReact from "google-map-react";
+import { ConfigurationServicePlaceholders } from "aws-sdk/lib/config_service_placeholders";
 
 interface ICoords {
   lat: number;
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚗</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriveCoords] = useState<ICoords>({ lng: 0, lat: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
   // @ts-ignore
   const onSuccess = ({
@@ -29,11 +38,20 @@ export const Dashboard = () => {
   }, []);
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      /* const geocoder = new google.maps.Geocoder();
+      geocoder.geocode(
+        {
+          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        },
+        (results, status) => {
+          console.log(status, results);
+        }
+      ); */
     }
   }, [map, maps, driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
     setMaps(maps);
   };
@@ -54,15 +72,7 @@ export const Dashboard = () => {
           }}
           bootstrapURLKeys={{ key: "AIzaSyDwu5TwKK2NE-TboTH693zFRrG37BB4Mu8" }}
         >
-          <div
-            // @ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-            //className="h-8 w-8 flex justify-center items-center rounded-full"
-            className="text-lg"
-          >
-            🚗
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
     </div>
